ea7e5f874519aeb29e42607be68cfcb7229609f7
RajKothari001 <rajkothari14364@gmail.com>
2025-06-06 12:20:58 +0530

    Update lca_dataflow.v

--- look ahead carry/lca_dataflow.v
+++ look ahead carry/lca_dataflow.v
@@ -3,14 +3,16 @@ module lca_dataflow(sum, cout, a, b, cin);
     input cin;
     output [7:0] sum;
     output cout;
-    wire [7:0] p, g, c;
+    wire [7:0] p, g;
+  	wire [8:0] c;	
 
     assign p = a ^ b;
     assign g = a & b;
     assign c[0] = cin;
     assign sum[0] = p[0] ^ cin;
-    assign c[7:1] = g[6:0] | (p[7:1] & c[6:0]);
+  	assign c[8:1] = g[7:0] | (p[7:0] & c[7:0]);
     assign sum[7:1] = p[7:1] ^ c[7:1];
+  	assign cout = c[8];
     // genvar i;
     // generate
     //     for (i = 1; i < 8; i = i + 1) begin : lca_adder
@@ -18,6 +20,4 @@ module lca_dataflow(sum, cout, a, b, cin);
     //         assign sum[i] = p[i] ^ c[i];
     //     end
     // endgenerate
-    assign cout = g[7] | (p[7] & c[7]);
-
 endmodule
